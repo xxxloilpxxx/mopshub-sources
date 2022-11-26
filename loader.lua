62893d297f6c8fe2a3d7c9f021535fd68fe86ed4
shyshai <68054842+xxxloilpxxx@users.noreply.github.com>
2022-11-26 21:26:56 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,14 +4,15 @@ local games, found = {
 	[10950541730] = "https://raw.githubusercontent.com/mopsfl/rbxmopshub/main/allofusaredead.lua",
 	[10950394697] = "https://raw.githubusercontent.com/mopsfl/rbxmopshub/main/allofusaredead.lua",
 	[2988554876] = "https://raw.githubusercontent.com/mopsfl/rbxmopshub/main/militarysimulator.lua",
+	[292439477] = "https://raw.githubusercontent.com/mopsfl/rbxmopshub/main/phantomforces.lua",
 }, false
 local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
 local Notify = AkaliNotif.Notify;
 
 getgenv().gameid = game.PlaceId
 
-for id,scriptstr in pairs(games) do
-	if id == getgenv().gameid then
+for id, scriptstr in pairs(games) do
+	if tonumber(id) == getgenv().gameid then
 		found = true
 		print(string.format("\n\n[mopsHub Loader]: Found script for gameid [%s]\n\n> Loading script from %s\n\n", getgenv().gameid,scriptstr))
 		local _s, _e = pcall(function() 
