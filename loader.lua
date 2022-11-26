41e6effe6ca37370a89d352d889c0e85ca28954d
shyshai <68054842+xxxloilpxxx@users.noreply.github.com>
2022-11-26 15:52:17 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,6 +3,7 @@ local games, found = {
 	[9759729519] = "https://raw.githubusercontent.com/mopsfl/rbxmopshub/main/allofusaredead.lua",
 	[10950541730] = "https://raw.githubusercontent.com/mopsfl/rbxmopshub/main/allofusaredead.lua",
 	[10950394697] = "https://raw.githubusercontent.com/mopsfl/rbxmopshub/main/allofusaredead.lua"
+	[2988554876] = "https://raw.githubusercontent.com/mopsfl/rbxmopshub/main/militarysimulator.lua"
 }, false
 local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
 local Notify = AkaliNotif.Notify;
