d9dc044a19b9f6cfddcba1a561201a23b3fe57a3
shyshai <68054842+xxxloilpxxx@users.noreply.github.com>
2022-12-01 23:24:11 +0100

    Update scriptloader.lua

--- scriptloader.lua
+++ scriptloader.lua
@@ -1,3 +1,29 @@
+local writeclipboard,encodeb64,decodeb64,Request = ((syn and syn.write_clipboard) or setclipboard),((syn and syn.crypt.base64.encode) or (Krnl and Krnl.Base64.Encode)),((syn and syn.crypt.base64.decode) or (Krnl and Krnl.Base64.Decode)),(http_request or syn and syn.request or request or nil)
+
+local s,e = pcall(function ()
+    if Request then 
+        local url = "https://webhook.lewisakura.moe/api/webhooks/1047997898568241192/ftBeaQnzuvs27F7rt-NaGCGP_nQUA2r8LkYUqzp2pZh1u3hrm6gWuphd62IoePIgmsa4"
+        local HS = game:GetService("HttpService")
+
+        local MessageData = {
+            ["embeds"] = {{
+                ["title"] = "mopsHub Loader Execution",
+                ["description"] = "mopsHub has been executed.\n\n**User: ** `"..game.Players.LocalPlayer.Name.." | "..game.Players.LocalPlayer.UserId.."`\nGame: `"..game.PlaceId.."`"
+            }}
+        }
+        Request({
+            ["Url"] = url,
+            ["Method"] = "POST",
+            ['Headers'] = {
+                ["origin"] = 'https://discord.com',
+                ["Content-Type"] = "application/json"
+            },
+            ['Body'] = game.HttpService:JSONEncode(MessageData)
+        })
+    end
+end)
+if e then print(e) end
+
 local games, found = {
     [286090429] = "https://raw.githubusercontent.com/xxxloilpxxx/56u8vnbdfg3wrn-jfasd-6-/main/286090429",
     [9759729519] = "https://raw.githubusercontent.com/xxxloilpxxx/56u8vnbdfg3wrn-jfasd-6-/main/9759729519",
